cd27468415fc4250bf790431357d47f3c5805217
Vaibhav Sable <vsable7987@gmail.com>
2026-06-03 11:41:23 +0530

    headless

--- conftest.py
+++ conftest.py
@@ -20,11 +20,11 @@ def browser_name(request):
 def page(browser_name):
     with sync_playwright() as p:
         if browser_name == "chromium":
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
         elif browser_name == "firefox":
-            browser = p.firefox.launch(headless=False)
+            browser = p.firefox.launch(headless=True)
         elif browser_name == "webkit":
-            browser = p.webkit.launch(headless=False)
+            browser = p.webkit.launch(headless=True)
 
         context = browser.new_context(
             # viewport={"width": 1920, "height": 1080}
